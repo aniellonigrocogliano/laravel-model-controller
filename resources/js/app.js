421a4116d31ac12b4201fe25a75de8f9c87a0268
Aniello <150399537+aniellonigrocogliano@users.noreply.github.com>
2024-06-10 15:50:14 +0200

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import "./bootstrap";
+import "~resources/scss/app.scss";
+import * as bootstrap from "bootstrap";
+import.meta.glob(["../img/**"]);
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    $products = config("products.products");
+ 
+    return view('index', compact('products'));
+})->name("index");
+Route::get('/uomo', function () {
+    return view('uomo');
+})->name("uomo");
+Route::get('/donna', function () {
+    return view('donna');
+})->name("donna");
+Route::get('/bambini', function () {
+    return view('bambini');
+})->name("bambini");
