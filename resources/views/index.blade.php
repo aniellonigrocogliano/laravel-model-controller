4c99b3e2cf21e1b842ea7aafc0523db1c1a2bcc7
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-06-10 20:30:51 +0200

    fix finale

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <section class="bg-section">
         <div class="container d-flex justify-content-between flex-wrap">
-            <h1 class="text-center">Prodotti</h1>
+            <h1 class="text-center">Lista film da database</h1>
             <div class="row g-3 wrap d-flex justify-content-between">
                 @foreach ($Movies as $Movies)
                     <div class="col-3 ">
