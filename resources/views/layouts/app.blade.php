d255ffd7c19e24ebf138957979ed7bf73153f4ea
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-06-10 20:29:14 +0200

    add: style star and flag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Lista film</title>
 
     <!-- Includiamo gli assets con la direttiva vite -->
     @vite('resources/js/app.js')
--- resources/views/partials/card.blade.php
+++ resources/views/partials/card.blade.php
@@ -11,8 +11,26 @@
     @php
         $date = date('d/m/Y', strtotime($Movies['date']));
     @endphp
-    <p class="card-text">Data di uscita: {{ $date }} </p>
-    <p class="card-text">Nazionalità: {{ $Movies['nationality'] }}</p>
-    <p class="card-text">Voto: {{ $Movies['vote'] }}</p>
+    <p class="card-text"><i class="fa-solid fa-calendar-days"></i> {{ $date }} </p>
+    @if ($Movies['nationality'] === 'american')
+        <p class="card-text"><i class="fa-solid fa-globe"></i> <img src="{{ Vite::asset('resources/img/us.png') }}"
+                class="" alt="{{ $Movies['nationality'] }}"></p>
+    @else
+        <p class="card-text"><i class="fa-solid fa-globe"></i> <img src="{{ Vite::asset('resources/img/gb.png') }}"
+                class="" alt="{{ $Movies['nationality'] }}"></p>
+    @endif
+    <p class="card-text"><i class="fa-solid fa-square-poll-vertical"></i>
+        @php
+            $vote = round($Movies['vote']);
+
+            for ($i = 0; $i < 10; $i++) {
+                if ($i < $vote) {
+                    echo '<i class="fa-solid fa-star"></i>';
+                } else {
+                    echo '<i class="fa-regular fa-star"></i>';
+                }
+            }
+
+        @endphp</p>
 </div>
 </div>
